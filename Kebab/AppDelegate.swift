62957cddc56fb370f48b3176d8110484e957f825
Oussama Siharde <oussama.siharde@live.fr>
2022-09-16 17:05:44 +0100

    Initial Commit

---
+++ Kebab/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Kebab
+//
+//  Created by Oussama Siharde on 9/16/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Kebab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Kebab
+//
+//  Created by Oussama Siharde on 9/16/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
