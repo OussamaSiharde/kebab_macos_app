814bf8c4475aa787ff85463b7e2a34395ac40964
Oussama Siharde <oussama.siharde@live.fr>
2022-09-17 03:41:31 +0100

    init commit

--- Kebab/ViewController.swift
+++ Kebab/ViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import Cocoa
+import Foundation
 
 class ViewController: NSViewController {
+    @IBOutlet var text_field1: NSTextFieldCell!
+    
+    @IBOutlet var text_field2: NSTextField!
 
+    @IBAction func button1(_ sender: Any) {
+        var text = text_field1.stringValue
+        text.removeAll(where: {$0.isPunctuation})
+        
+        text_field2.stringValue = text.split{$0 == " "}.map({ $0.lowercased() }).joined(separator: "-")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
